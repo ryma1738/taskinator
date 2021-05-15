50d1a5da2da56dc07a0d4bd0e2036b27f9630cef
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-05-15 17:29:28 -0600

    Refactered form and completed form UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,36 @@
 var taskToDo = document.querySelector("#task-to-do");
 var formEl = document.querySelector("#task-form");
 
-function addTask(event) {
+function taskFormHandler(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value; 
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    var taskObject = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
+    if (!taskObject.name || !taskObject.type) {
+        alert("Please enter a valid name and type selection for your task!")
+    }
+    else {
+        addTask(taskObject);
+    }
+    formEl.reset();
+    
+    
+}
+
+function addTask(taskObject) {
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskObject.name + "</h3><span class='task-type'>" + taskObject.type + "</span>";
 
     taskItemEl.appendChild(taskInfoEl);
-    taskToDo.appendChild(taskItemEl);
+    taskToDo.appendChild(taskItemEl); 
 }
 
-formEl.addEventListener("submit", addTask); 
+formEl.addEventListener("submit", taskFormHandler); 
